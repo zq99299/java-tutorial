71ef5d9373989f5b298265b951e0bdfd1395fbdd
zhuqiang <zhuqiang@tidebuy.net>
2019-02-15 11:25:07 +0800

    学习 java 侧边栏分组优化完成

--- docs/java/index.js
+++ docs/java/index.js
@@ -70,34 +70,58 @@ module.exports = () => {
             collapsable: true,
             children: [
                 ['javaoo/','概述'],
-                'javaoo/classes.md',
-                'javaoo/classdecl.md',
-                'javaoo/variables.md',
-                'javaoo/methods.md',
-                'javaoo/constructors.md',
-                'javaoo/arguments.md',
-                'javaoo/objects.md',
-                'javaoo/objectcreation.md',
-                'javaoo/usingobject.md',
-                'javaoo/more.md',
-                'javaoo/returnvalue.md',
-                'javaoo/thiskey.md',
-                'javaoo/accesscontrol.md',
-                'javaoo/classvars.md',
-                'javaoo/initial.md',
-                'javaoo/summaryclasses.md',
-                'javaoo/qande/creating-questions.md',
-                'javaoo/qande/objects-questions.md',
-                'javaoo/nested.md',
-                'javaoo/innerclasses.md',
-                'javaoo/localclasses.md',
-                'javaoo/anonymousclasses.md',
-                'javaoo/lambdaexpressions.md',
-                'javaoo/methodreferences.md',
-                'javaoo/whentouse.md',
-                'javaoo/qande/nested-questions.md',
-                'javaoo/enum.md',
-                'javaoo/qande/enum-answers.md'
+                {
+                    title: "类",
+                    collapsable: true,
+                    children: [
+                      'javaoo/classes.md',
+                      'javaoo/classdecl.md',
+                      'javaoo/variables.md',
+                      'javaoo/methods.md',
+                      'javaoo/constructors.md',
+                      'javaoo/arguments.md'
+                    ]
+                },
+                {
+                    title: "对象",
+                    collapsable: true,
+                    children: [
+                      'javaoo/objects.md',
+                      'javaoo/objectcreation.md'
+                    ]
+                },
+                {
+                    title: "类的更多信息",
+                    collapsable: true,
+                    children: [
+                      'javaoo/usingobject.md',
+                      'javaoo/more.md',
+                      'javaoo/returnvalue.md',
+                      'javaoo/thiskey.md',
+                      'javaoo/accesscontrol.md',
+                      'javaoo/classvars.md',
+                      'javaoo/initial.md',
+                      'javaoo/summaryclasses.md',
+                      'javaoo/qande/creating-questions.md',
+                      'javaoo/qande/objects-questions.md'
+                    ]
+                },
+                {
+                    title: "嵌套类",
+                    collapsable: true,
+                    children: [
+                      'javaoo/nested.md',
+                      'javaoo/innerclasses.md',
+                      'javaoo/localclasses.md',
+                      'javaoo/anonymousclasses.md',
+                      'javaoo/lambdaexpressions.md',
+                      'javaoo/methodreferences.md',
+                      'javaoo/whentouse.md',
+                      'javaoo/qande/nested-questions.md',
+                      'javaoo/enum.md',
+                      'javaoo/qande/enum-answers.md'
+                    ]
+                }
             ]
         },
         {
@@ -118,25 +142,37 @@ module.exports = () => {
             collapsable: true,
             children: [
                 ['iandi/','概述'],
-                'iandi/createinterface.md',
-                'iandi/interface_def.md',
-                'iandi/usinginterface.md',
-                'iandi/interface_as_type.md',
-                'iandi/nogrow.md',
-                'iandi/defaultmethods.md',
-                'iandi/summary_interface.md',
-                'iandi/qande/interfaces_questions.md',
-                'iandi/subclasses.md',
-                'iandi/multipleinheritance.md',
-                'iandi/override.md',
-                'iandi/polymorphism.md',
-                'iandi/hidevariables.md',
-                'iandi/super.md',
-                'iandi/objectclass.md',
-                'iandi/final.md',
-                'iandi/abstract.md',
-                'iandi/summaryinherit.md',
-                'iandi/qande/inherit_questions.md',
+                {
+                    title: "接口",
+                    collapsable: true,
+                    children: [
+                      'iandi/createinterface.md',
+                      'iandi/interface_def.md',
+                      'iandi/usinginterface.md',
+                      'iandi/interface_as_type.md',
+                      'iandi/nogrow.md',
+                      'iandi/defaultmethods.md',
+                      'iandi/summary_interface.md',
+                      'iandi/qande/interfaces_questions.md'
+                    ]
+                },
+                {
+                    title: "继承",
+                    collapsable: true,
+                    children: [
+                      'iandi/subclasses.md',
+                      'iandi/multipleinheritance.md',
+                      'iandi/override.md',
+                      'iandi/polymorphism.md',
+                      'iandi/hidevariables.md',
+                      'iandi/super.md',
+                      'iandi/objectclass.md',
+                      'iandi/final.md',
+                      'iandi/abstract.md',
+                      'iandi/summaryinherit.md',
+                      'iandi/qande/inherit_questions.md'
+                    ]
+                }
             ]
         },
         {
@@ -144,21 +180,33 @@ module.exports = () => {
             collapsable: true,
             children: [
                 ['data/','概述'],
-                'data/numbers.md',
-                'data/numberclasses.md',
-                'data/numberformat.md',
-                'data/beyondmath.md',
-                'data/numbersummary.md',
-                'data/qande/numbers_questions.md',
+                {
+                    title: "数字 Number",
+                    collapsable: true,
+                    children: [
+                      'data/numbers.md',
+                      'data/numberclasses.md',
+                      'data/numberformat.md',
+                      'data/beyondmath.md',
+                      'data/numbersummary.md',
+                      'data/qande/numbers_questions.md'
+                    ]
+                },
                 'data/characters.md',
-                'data/strings.md',
-                'data/converting.md',
-                'data/manipstrings.md',
-                'data/comparestrings.md',
-                'data/buffers.md',
-                'data/stringsummary.md',
+                {
+                    title: "字符串 String",
+                    collapsable: true,
+                    children: [
+                      'data/strings.md',
+                      'data/converting.md',
+                      'data/manipstrings.md',
+                      'data/comparestrings.md',
+                      'data/buffers.md',
+                      'data/stringsummary.md'
+                    ]
+                },
                 'data/autoboxing.md',
-                'data/qnde/characters_questions.md',
+                'data/qnde/characters_questions.md'
             ]
         },
         {
@@ -174,18 +222,30 @@ module.exports = () => {
                 'generics/boundedTypeParams.md',
                 'generics/inheritance.md',
                 'generics/genTypeInference.md',
-                'generics/wildcards.md',
-                'generics/upperBounded.md',
-                'generics/unboundedWildcards.md',
-                'generics/lowerBounded.md',
-                'generics/subtyping.md',
-                'generics/capture.md',
-                'generics/wildcardGuidelines.md',
-                'generics/erasure.md',
-                'generics/genTypes.md',
-                'generics/genMethods.md',
-                'generics/bridgeMethods.md',
-                'generics/nonReifiableVarargsType.md',
+                {
+                    title: "通配符",
+                    collapsable: true,
+                    children: [
+                      'generics/wildcards.md',
+                      'generics/upperBounded.md',
+                      'generics/unboundedWildcards.md',
+                      'generics/lowerBounded.md',
+                      'generics/subtyping.md',
+                      'generics/capture.md',
+                      'generics/wildcardGuidelines.md'
+                    ]
+                },
+                {
+                    title: "类型擦除",
+                    collapsable: true,
+                    children: [
+                      'generics/erasure.md',
+                      'generics/genTypes.md',
+                      'generics/genMethods.md',
+                      'generics/bridgeMethods.md',
+                      'generics/nonReifiableVarargsType.md',
+                    ]
+                },
                 'generics/restrictions.md',
                 'generics/qande/generics_questions.md'
             ]
